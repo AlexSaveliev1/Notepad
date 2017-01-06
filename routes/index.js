2dd6d0f05fb695607ecafd5f3d76d32846bf9f90
Sasha Saveliev <airbasic4@gmail.com>
2017-01-06 15:43:36 +0200

    Set view for: index,login,register; Start make validation for register form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express=require('express');
 var router=express.Router();
 
+var conectionCounter=0; // Counter for connections on port
+
 // Get Homepage
 router.get('/',function(req,res){
     res.render('index')
+    console.log ('Connection counter: '+ ++conectionCounter);
 })
 
 module.exports=router;

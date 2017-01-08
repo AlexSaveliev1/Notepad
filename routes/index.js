7e785ccd6586d82a357341b40f8323425c4542f5
Sasha Saveliev <airbasic4@gmail.com>
2017-01-08 14:27:19 +0200

    Finish registration system

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
-var express=require('express');
-var router=express.Router();
-
-var conectionCounter=0; // Counter for connections on port
+var express = require('express');
+var router = express.Router();
+var uname=require('./users.js')
 
 // Get Homepage
-router.get('/',function(req,res){
-    res.render('index')
-    console.log ('Connection counter: '+ ++conectionCounter);
-})
+router.get('/', ensureAuthenticated, function(req, res){
+    res.render('index');
+});
 
-module.exports=router;
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+
+        return next();
+    } else {
+        req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
+
+module.exports = router;
